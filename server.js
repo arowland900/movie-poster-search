9b794d0b132346218b3e4165e91c63cbca3e7d78
Alexander Rowland <arowland900@gmail.com>
2018-06-22 16:09:19 -0700

    shows all posters found in database search

--- server.js
+++ server.js
@@ -12,24 +12,19 @@ const apiClient = axios.create()
 
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-    console.log("REQUEST RECEIVED, CONTACTING MOVIE DATABASE...")
-    const apiUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.API_KEY}&`
-    apiClient({ method: "get", url: apiUrl }).then((dataThatCameBack) => {
-      console.log(dataThatCameBack.data)
-      res.json({ message: "something is working"})
-    })
-})
-
 app.get('/search/:term', (req, res) => {
     var term = req.params.term
 
     console.log("REQUEST RECEIVED, CONTACTING MOVIE DATABASE...")
-    const apiUrl = `http://www.omdbapi.com/?t=${term}&apikey=${process.env.API_KEY}&`
+    const apiUrl = `http://www.omdbapi.com/?s=${term}&apikey=${process.env.API_KEY}&`
     apiClient({ method: "get", url: apiUrl }).then((dataThatCameBack) => {
-      console.log(dataThatCameBack.data["Poster"])
-      var imgUrl = dataThatCameBack.data["Poster"]
-      res.send(`<img src='${imgUrl}' />`)
+    //   console.log(dataThatCameBack.data["Search"])
+        results = ''
+        dataThatCameBack.data["Search"].forEach((r) => {
+            let imgUrl = r["Poster"]
+            results += `<img src="${imgUrl}">`
+        })
+        res.send(results)
     })
 })
 
